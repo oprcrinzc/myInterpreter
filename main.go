743a10450bf38984f3ecdfa30b9ea3b407dd428a
oprc <chisainux@gmail.com>
2025-01-23 22:29:06 +0700

    a little move

--- main.go
+++ main.go
@@ -6,13 +6,43 @@ import (
 )
 
 var fileExtension string = ".rbc"
+var instructionSet [][]byte
+
+func check(e error) {
+	if e != nil {
+		fmt.Println("Error!: " + e.Error())
+		panic(e)
+	}
+}
+
+func LoadInstructionSet(data []byte) {
+	var instruction []byte
+	for i, e := range data {
+		if e == 32 || (e == 10) {
+			// instruction = instruction[len(instruction)-1:]
+			instructionSet = append(instructionSet, instruction)
+			instruction = nil
+			continue
+		}
+		instruction = append(instruction, e)
+		if i == len(data)-1 {
+			instructionSet = append(instructionSet, instruction)
+		}
+	}
+}
 
 func main() {
 	args := os.Args
-	if fileName := args[1]; fileName[4:] != fileExtension {
+	if fileName := args[1]; fileName[len(fileName)-4:] != fileExtension {
+		fmt.Println(fileName)
 		fmt.Println("i prefer .rbc file !")
 		return
 	}
 
-	fmt.Println("ok")
+	data, err := os.ReadFile(args[1])
+	check(err)
+	fmt.Println(string(data))
+	fmt.Println(data)
+	LoadInstructionSet(data)
+	fmt.Println(instructionSet)
 }
